15b355970e31f805662036edfbf93069c9bcef17
Stanislau Karbanovich <karbanovichstanislav@gmail.com>
2025-05-25 18:36:19 +0200

    feat: webpack

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+import '../scss/style.scss'; // подключаем стили
+
+// подключаем остальные модули
+import './burger.js';
+import './cart.js';
+import './swiper.js';
+import './dom.js';
+import './scroll.js';
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/script.js', // основной JS-файл
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // очищать dist перед сборкой
+  },
+  module: {
+    rules: [
+      {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
+        type: 'asset/resource',
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    open: true,
+    hot: true,
+  },
+  mode: 'development', // поменяй на 'production' при финальной сборке
+};
